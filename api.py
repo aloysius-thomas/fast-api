88d4c9642eb7d91b9a8fa1ea2ea8e01e1b6cff2b
aloysius thomas <aloysius.thomas@nthindex.com>
2022-10-22 14:01:55 +0530

    Url Query

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-import imp
 from fastapi import FastAPI, Path
+from typing import Optional
 
 
 app = FastAPI()
@@ -24,3 +24,11 @@ def index():
 @app.get("/get-students/{student_id}")
 def get_students(student_id: int = Path(None,  description='ID of the students ', gt=0)):
     return students[student_id]
+
+
+@app.get("/get-student-by-name")
+def get_student_by_name(*, name: Optional[str] = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"message": "data not fount"}
