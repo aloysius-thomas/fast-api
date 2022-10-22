52fe117bb7b57ca42121f3d2f91c620e774e52ec
aloysius thomas <aloysius.thomas@nthindex.com>
2022-10-22 13:46:24 +0530

    Params

--- api.py
+++ api.py
@@ -1,9 +1,26 @@
 import imp
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "Aloysius",
+        "age": 28
+    },
+    2: {
+        "name": "Alex",
+        "age": 26
+    }
+}
+
+
 @app.get("/")
 def index():
     return {"message": "Hello world"}
+
+
+@app.get("/get-students/{student_id}")
+def get_students(student_id: int = Path(None,  description='ID of the students ', gt=0)):
+    return students[student_id]
